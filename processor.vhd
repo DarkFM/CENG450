c7dc1b69690db17cc06abe001e132e0c5f41b6fe
DarkFM <c.bestmann@hotmail.com>
2017-03-10 21:10:11 -0800

    connected modules including decode and reg_file

--- processor.vhd
+++ processor.vhd
@@ -1,20 +1,20 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    17:10:15 03/08/2017 
--- Design Name: 
--- Module Name:    processor - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Company:
+-- Engineer:
 --
--- Dependencies: 
+-- Create Date:    17:10:15 03/08/2017
+-- Design Name:
+-- Module Name:    processor - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool versions:
+-- Description:
 --
--- Revision: 
+-- Dependencies:
+--
+-- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments: 
+-- Additional Comments:
 --
 ----------------------------------------------------------------------------------
 library IEEE;
@@ -60,43 +60,62 @@ architecture Behavioral of processor is
         	data: OUT std_logic_vector(15 downto 0)
 	  );
     END component;
-	 
+
 	component FBUF is
 		Port ( clk: in STD_LOGIC ;
 			instr_in : in  STD_LOGIC_VECTOR (15 downto 0);
 			instr_out : out  STD_LOGIC_VECTOR (15 downto 0)
 		);
 	end component;
-	
+
 	component decode_stage is
 		Port(
 			instr_in : in std_logic_vector(15 downto 0);
-			write_en : in std_logic;
-			write_address: in std_logic_vector(2 downto 0);
-			data_in : std_logic_vector(15 downto 0);
-			clk : in std_logic;
-			reset : in std_logic;
+			OUT_OP_CODE : out std_logic_vector(6 downto 0);
 
-			OP_CODE : out std_logic_vector(6 downto 0);
+            rd_index1 : out std_logic_vector(2 downto 0);
+            rd_index2 : out std_logic_vector(2 downto 0);
 
-			-- needed for A instruvctions
-			OUT_VAL_1 : out std_logic_vector(15 downto 0);
-			OUT_VAL_2 : out std_logic_vector(15 downto 0);
 
-			-- needed for the shift offset
-			OUT_C1 : out std_logic_vector(3 downto 0)
+            -- needed for the shift offset
+            OUT_C1 : out std_logic_vector(3 downto 0);
 
+    		OUT_RA_index : out std_logic_vector(2 downto 0)
 		);
 	end component;
-	 
+
+    component register_file is
+		port (
+			rst : in std_logic;
+			clk : in std_logic;
+
+			rd_address_1: in std_logic_vector(2 downto 0);
+			data_out_1: out std_logic_vector(15 downto 0);
+
+			rd_address_2: in std_logic_vector(2 downto 0);
+			data_out_2: out std_logic_vector(15 downto 0);
+
+			wr_address: in std_logic_vector(2 downto 0);
+			data_in: in std_logic_vector(15 downto 0);
+			reg_wr_en: in std_logic
+		);
+	end component;
+
+
 	signal PC_out: std_logic_vector(6 downto 0);
 	signal INSTR_TO_FBUF: std_logic_vector(15 downto 0);
 	signal FBUF_TO_DECODE: std_logic_vector(15 downto 0);
-	
+
 	--signals for FBUF to REG_FILE
---	SIGNAL op_code: std_logic_vector(6 downto 0);
+	SIGNAL S_op_code: std_logic_vector(6 downto 0);
+    signal S_RB: std_logic_vector(2 downto 0);
+    signal S_RC: std_logic_vector(2 downto 0);
 	--alias RA is STD_LOGIC_VECTOR(2 DOWNTO 0);
 	--alias RA: STD_LOGIC_VECTOR(2 DOWNTO 0);
+
+    -- signals for decode to reg file
+    signal index1 : std_logic_vector(2 downto 0);
+    signal index2 : std_logic_vector(2 downto 0);
 
 
 begin
@@ -115,34 +134,44 @@ begin
 			clk => clock,
 			data => INSTR_TO_FBUF
 		);
-			
+
 	fetch_buffer: FBUF
 		port map(
 			clk => clock,
 			instr_in => INSTR_TO_FBUF,
 			instr_out => FBUF_TO_DECODE
-		); 	
+		);
 
 	decode: decode_stage
 	Port map
 	(
 		instr_in => FBUF_TO_DECODE,
-		write_en => '0',
-		write_address => (others => '0'),
-		data_in => (others => '0'),
-		clk => clock,
-		reset => reset,
-
-		OP_CODE => op_code,
+		OUT_OP_CODE => op_code,
 
 		-- needed for A instruvctions
-		OUT_VAL_1 => OUT_VAL_1,
-		OUT_VAL_2 =>OUT_VAL_2,
-		
+		rd_index1 => index1,
+		rd_index2 => index2,
+
 		-- needed for the shift offset
 		OUT_C1 => OUT_C1
 
 	);
 
-end Behavioral;
 
+    reg_file: register_file
+    	port map(
+    		rst  => reset,
+    		clk => clk,
+
+    		rd_address_1 => index1,
+    		data_out_1 => OUT_VAL_1,
+
+    		rd_address_2 => index2,
+    		data_out_2 => OUT_VAL_2,
+
+    		wr_address => (others => '0'),
+    		data_in => (others => '0'),
+    		reg_wr_en => '0''
+    	);
+
+end Behavioral;
