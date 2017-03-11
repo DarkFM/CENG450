59894e7f1c91f923e49ae49df92e10c1686466e0
DarkFM <c.bestmann@hotmail.com>
2017-03-10 23:09:06 -0800

    renamed ports

--- DBUF.vhd
+++ DBUF.vhd
@@ -13,15 +13,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity DBUF is
     Port ( clk: in STD_LOGIC ;
-            IN_ra: in std_logic_vector(15 downto 0);
+            IN_ra_index: in std_logic_vector(3 downto 0);
             IN_rb: in std_logic_vector(15 downto 0);
-			IN_rx : in  STD_LOGIC_VECTOR (2 downto 0);
+            IN_rc: in std_logic_vector(15 downto 0);
+			IN_c1 : in  STD_LOGIC_VECTOR (2 downto 0);
             IN_op_code: in std_logic_vector(6 downto 0);
 
-            OUT_ra: out std_logic_vector(15 downto 0);
+            OUT_ra_index: out std_logic_vector(3 downto 0);
             OUT_rb: out std_logic_vector(15 downto 0);
-			OUT_rx : out  STD_LOGIC_VECTOR (2 downto 0);
-            OUT_op_code: out std_logic_vector(6 downto 0);
+            OUT_rc: out std_logic_vector(15 downto 0);
+			OUT_c1 : out  STD_LOGIC_VECTOR (2 downto 0);
+            OUT_op_code: out std_logic_vector(6 downto 0)
+        );
 			-- instr_out : out  STD_LOGIC_VECTOR (15 downto 0));
 end DBUF;
 
@@ -30,13 +33,14 @@ architecture Behavioral of DBUF is
 
 begin
 
-	process(clk,instr_in)
+	process(clk)
 	begin
 		if rising_edge(clk) and clk'event  then
 
-            OUT_ra <= IN_ra;
+            OUT_ra_index <= IN_ra_index;
             OUT_rb <= IN_rb;
-            OUT_rx <= IN_rx;
+            OUT_rc <= IN_rc;
+            OUT_c1 <= IN_c1;
             OUT_op_code <= IN_op_code;
 		end if;
 	END PROCESS;
